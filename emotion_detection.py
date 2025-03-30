d47f156d23d69467c867d74a3e2432281d3136bc
Jefferson Speck <jeffersonspeck@msn.com>
2025-03-30 15:19:45 -0300

    acertos

---
+++ emotion_detection.py
@@ -0,0 +1,53 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        result = response.json()
+        emotions = result['document']['emotion']
+
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+
+        emotion_scores = {
+            'anger': anger,
+            'disgust': disgust,
+            'fear': fear,
+            'joy': joy,
+            'sadness': sadness
+        }
+        dominant = max(emotion_scores, key=emotion_scores.get)
+
+        return {
+            'anger': anger,
+            'disgust': disgust,
+            'fear': fear,
+            'joy': joy,
+            'sadness': sadness,
+            'dominant_emotion': dominant
+        }
+    else:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
---
+++ server.py
@@ -0,0 +1,30 @@
+"""File providing the server and route handlers."""
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emotion_detetion_action():
+    """Emotion detection handler."""
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+    emotions = ''.join([f"'{key}': {value}, " for key, value in response.items()])
+    if response['dominant_emotion'] is None:
+        text = 'Invalid text! Please try again!.'
+    else:
+        text = f'''
+            For the given statement, the system response
+            is {emotions}. The dominant emotion 
+            is <b>{response['dominant_emotion']}</b>.
+        '''
+    return text
+
+@app.route("/")
+def render_index_page():
+    """Index page handler"""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
